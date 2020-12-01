6e7a6ff9a92a6ec34060926cbeb10568045ccc9f
Andrii <drake.austin.0303@gmail.com>
2020-12-01 13:09:30 +0200

    Added NodeListView

--- note/models.py
+++ note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField("title", max_length=200)
+    body = models.TextField("text")
+    created = models.DateTimeField("created")
+    updated = models.DateTimeField("updated")
+    image = models.ImageField("image", upload_to="images")
+
+    def __str__(self):
+        return self.title
--- note/views.py
+++ note/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Note
+
+
+class NoteListView(ListView):
+    model = Note
+    #def get_context_data(self, *, object_list=None, **kwargs):
+    context_object_name = "notes"
+    template_name = "note/main.html"
