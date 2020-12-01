bd573df6b6e811a70db390ad7bb3d689dd83d7c9
Andrii <drake.austin.0303@gmail.com>
2020-12-01 15:11:35 +0200

    added delete view

--- note/urls.py
+++ note/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.NoteListView.as_view())
+    path("", views.NoteListView.as_view(), name="note-list"),
+    path("<int:pk>/delete/", views.NoteDeleteView.as_view(), name="delete-note")
 ]
--- note/views.py
+++ note/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView
+from .models import Note
 
 from .models import Note
 
 
 class NoteListView(ListView):
     model = Note
-    #def get_context_data(self, *, object_list=None, **kwargs):
+    # def get_context_data(self, *, object_list=None, **kwargs):
     context_object_name = "notes"
     template_name = "note/main.html"
+
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    template_name = "note/delete.html"
