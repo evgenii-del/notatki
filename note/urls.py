f27abbcaa8ba530a5ab8b4720114c1e102c193f3
Andrii <drake.austin.0303@gmail.com>
2020-12-13 19:16:44 +0200

    added requirements.txt

--- note/urls.py
+++ note/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.NoteListView.as_view(), name="notes-list"),
+    #path("", views.NoteListView.as_view(), name="notes-list"),
     path("create/", views.NoteCreateView.as_view(), name="notes-create"),
     path("<int:pk>/", views.NoteDetailView.as_view(), name="notes-detail"),
     path("<int:pk>/update/", views.NoteUpdateView.as_view(), name="notes-update"),
     path("<int:pk>/delete/", views.NoteDeleteView.as_view(), name="notes-delete"),
-    path("search/", views.NotesSearchList.as_view(), name="notes-search")
+    path("", views.NotesSearchList.as_view(), name="notes-list")
 ]
